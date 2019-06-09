dcd6baee6ae3aa64b3636884d6d08b8b8cb69f8e
Manu Phatak <manu.phatak@procore.com>
2019-06-08 20:06:01 -0700

    establish static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should be root' do
+    get root_url
+    assert_response :success
+    assert_select 'title', 'Home | Toy App'
+  end
+
+  test 'should get home' do
+    get static_pages_home_url
+    assert_response :success
+    assert_select 'title', 'Home | Toy App'
+  end
+
+  test 'should get help' do
+    get static_pages_help_url
+    assert_response :success
+    assert_select 'title', 'Help | Toy App'
+  end
+
+  test 'should get about' do
+    get static_pages_about_url
+    assert_response :success
+    assert_select 'title', 'About | Toy App'
+  end
+
+  test 'should get contact' do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', 'Contact | Toy App'
+  end
+end
