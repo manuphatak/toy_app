9bc67bd25409e4769b015a468dab02112f1e02a5
Manu Phatak <manu.phatak@procore.com>
2019-06-14 11:06:47 -0700

    fix coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,12 @@
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require 'simplecov'
+SimpleCov.start 'rails'
 require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
+
 module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
