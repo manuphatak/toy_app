231f65ed7cb679c0b8202a8cdf7cd5ca3d2a97f1
Manu Phatak <manu.phatak@procore.com>
2019-06-14 10:26:59 -0700

    use static routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   resources :microposts
   resources :users
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,25 +10,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get home' do
-    get static_pages_home_url
+    get home_url
     assert_response :success
     assert_select 'title', 'Toy App'
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select 'title', 'Help | Toy App'
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select 'title', 'About | Toy App'
   end
 
   test 'should get contact' do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select 'title', 'Contact | Toy App'
   end
