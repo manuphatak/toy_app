3955a62e42f4bde26ccd6b0da836a3c1b2fb61f9
Manu Phatak <manu.phatak@procore.com>
2019-06-15 11:15:57 -0700

    add gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def gravatar_for(user, klass: 'gravatar', default: 'robohash', size: 80, rating: 'g')
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    query = { s: size, d: default, r: rating }.to_query
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?#{query}"
+    image_tag(gravatar_url, alt: user.name, class: klass)
+  end
 end
