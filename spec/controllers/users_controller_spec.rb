dd6583305afd66b0313152c93461a398046dc20a
Manu Phatak <manu.phatak@procore.com>
2019-06-15 21:40:23 -0700

    annotate tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
