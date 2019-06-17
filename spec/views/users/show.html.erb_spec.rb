5db1b07f7fc7dea2736f9a7affba16bf739681ba
Manu Phatak <manu.phatak@procore.com>
2019-06-16 22:02:15 -0700

    fix test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  let!(:user) { assign(:user, build_stubbed(:user, name: 'Test User', microposts: build_list(:micropost, 5))) }
+  let!(:user) { assign(:user, create(:user, :with_posts, posts_count: 5)) }
 
   it 'renders attributes in <p>' do
     render
 
     aggregate_failures do
-      expect(rendered).to have_selector format('h1>img[alt=%p]', 'Test User')
+      expect(rendered).to have_selector format('h1>img[alt=%p]', user.name)
       expect(rendered).to have_selector 'p', text: user.email
       user.microposts.each do |micropost|
         expect(rendered).to have_selector 'ul>li', text: micropost.content
