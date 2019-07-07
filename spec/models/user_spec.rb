fd80bd238100172755ceece233c51a7d1f6e40ff
Manu Phatak <manu.phatak@procore.com>
2019-07-06 18:15:35 -0700

    fix test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'email should be a valid email address' do
-      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses = %w[user22@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
       aggregate_failures do
         valid_addresses.each do |valid_address|
           stubbed_user.email = valid_address
