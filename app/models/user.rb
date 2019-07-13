891edd0dba178bc09183d7642aee9389a589ddbd
Manu Phatak <manu.phatak@procore.com>
2019-07-12 20:32:51 -0700

    add password reset feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,8 +37,9 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :timeoutable, :recoverable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable, :lockable, :confirmable
+  # :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable, :lockable, :confirmable,
+         :recoverable
 
   has_many :microposts, dependent: :destroy
 
