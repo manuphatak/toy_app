70c5c180a2d03429b4bc2c8884532fce4659e09a
Manu Phatak <manu.phatak@procore.com>
2019-08-09 11:22:23 -0700

    fix migrations

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -8,12 +8,16 @@
 #  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :integer
+#  user_id    :bigint
 #
 # Indexes
 #
 #  index_microposts_on_user_id                 (user_id)
 #  index_microposts_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 
 FactoryBot.define do
